5e8a366b4f98b27fc76ba990737df3ff270b4e3e
Jeremy Hummel <jmhummel@gmail.com>
2019-05-28 17:36:04 -0700

    Simple test loop

--- connect_4/connect_4.py
+++ connect_4/connect_4.py
@@ -3,29 +3,33 @@ import numpy as np
 
 class State:
     def __init__(self):
-        self.board = np.zeros((2, 7, 6), dtype=bool)
-        self.current_player = False
+        self.board = np.zeros((2, 6, 7), dtype=bool)
+        self.current_player = 0
 
     def get_valid_actions(self):
         valid_actions = np.ones((7,), dtype=bool)
         for i in range(7):
             if self.board[0, 0, i] or self.board[1, 0, i]:
                 valid_actions[i] = False
+        return valid_actions
 
     def take_action(self, i):
         for j in reversed(range(6)):
             if not self.board[0, j, i] and not self.board[1, j, i]:
                 self.board[self.current_player, j, i] = True
                 break
-        self.current_player = not self.current_player
+        self.current_player = 1 if self.current_player == 0 else 0
+
+    def is_game_over(self):
+        return not self.get_valid_actions().any()
 
     def __repr__(self):
         s = ''
         for j in range(6):
             for i in range(7):
-                if self.board[False, j, i]:
+                if self.board[0, j, i]:
                     s += '🔴'
-                elif self.board[True, j, i]:
+                elif self.board[1, j, i]:
                     s += '🔵'
                 else:
                     s += '⚫'
---
+++ connect_4/tests/test_connect_4.py
@@ -0,0 +1,23 @@
+import random
+import unittest
+
+import connect_4
+
+
+class TestInitialState(unittest.TestCase):
+    def setUp(self) -> None:
+        self.state = connect_4.connect_4.State()
+
+    def test_print(self):
+        print(self.state)
+        while not self.state.is_game_over():
+            print(self.state.get_valid_actions())
+            random_action = random.randrange(7)
+            while not self.state.get_valid_actions()[random_action]:
+                random_action = random.randrange(7)
+            self.state.take_action(random_action)
+            print(self.state)
+
+
+if __name__ == '__main__':
+    unittest.main()
