07deab8c8b9e44670caa647a04e700a417067a14
Jeremy Hummel <jhummel@onica.com>
2019-05-29 00:30:06 -0700

    Implement full Connect 4 state

--- connect_4/tests/test_connect_4.py
+++ connect_4/tests/test_connect_4.py
@@ -11,7 +11,7 @@ class TestInitialState(unittest.TestCase):
     def test_print(self):
         print(self.state)
         while not self.state.is_game_over():
-            print(self.state.get_valid_actions())
+            # print(self.state.get_valid_actions())
             random_action = random.randrange(7)
             while not self.state.get_valid_actions()[random_action]:
                 random_action = random.randrange(7)
